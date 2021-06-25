a2798b9a6ab187d2a18b0c09384594080ebfbe81
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-06-26 01:10:55 +0300

    адаптивная картинка в хедере и адаптивный блок посты

---
+++ HW2-gulp-scss/src/js/header/navigation.js
@@ -0,0 +1,12 @@
+(function () {
+   'use strict'
+   
+   const menuBurger = document.querySelector('.burger');
+   const menuList = document.querySelector('.menu');
+   
+   // Бургер меню
+   menuBurger.addEventListener('click', function(){
+      this.children[0].classList.toggle('burger__line--open');
+      menuList.classList.toggle('menu--open');
+   })
+})()
