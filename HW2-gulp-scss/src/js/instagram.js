103530346c0079ea19aa67ddd88a7b71c3c8d121
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-07-01 16:00:30 +0300

    добавила изменения заголовка при ресайзе

--- HW2-gulp-scss/src/js/instagram.js
+++ HW2-gulp-scss/src/js/instagram.js
@@ -1,13 +1,18 @@
 (function () {
    'use strict'
-   
+
    const instagramTitle = document.querySelector('.instagram__title');
+   changeTitle();
+
+   window.addEventListener('resize', changeTitle);
 
    // Изменение текста заголовка для маленьких экранов
-   if (document.documentElement.clientWidth <= 390) {
-      instagramTitle.innerHTML = 'Latest Instagram Shot';
-   } else {
-      instagramTitle.innerHTML = 'Latest Instagram Shots';
-   };
- 
+   function changeTitle() {
+      if (document.documentElement.clientWidth <= 390) {
+         instagramTitle.innerHTML = 'Latest Instagram Shot';
+      } else {
+         instagramTitle.innerHTML = 'Latest Instagram Shots';
+      };
+   }
+
 })();
